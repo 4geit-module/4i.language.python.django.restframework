c09e9a8e4a3ee78971942d8fa0361bbf00a99f9d
Caner Candan <caner@candan.fr>
2015-03-30 00:06:33 +0200

    + added single_app test

---
+++ tests/single_app/output/myapp/views.py
@@ -0,0 +1,15 @@
+# xyz
+# Copyright (C) 2014  xyz developers <admin@localhost.lh> (see AUTHORS)
+#
+# All rights reserved.
+
+from rest_framework import viewsets
+from . import serializers
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.utils.translation import ugettext_lazy as _
+from django.views import generic
+from . import models, forms
+
