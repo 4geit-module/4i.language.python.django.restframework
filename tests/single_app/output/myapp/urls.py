cc5cc106f7e94636c162ceda7899b5a7d7b56d5b
Caner Candan <caner@candan.fr>
2015-03-31 00:09:30 +0200

    + tests/single_app/output/myapp/urls.py

---
+++ tests/single_app/output/myapp/urls.py
@@ -0,0 +1,11 @@
+# xyz
+# Copyright (C) 2014  xyz developers <admin@localhost.lh> (see AUTHORS)
+#
+# All rights reserved.
+
+from myapp import views as myapp_views
+from rest_framework import routers
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView, DetailView, ListView
+from . import models, views
+
